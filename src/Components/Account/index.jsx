d47648c5b85c075a9429b56ebe9bacf7f7244026
Ham <ham@gmail.com>
2023-06-07 00:02:37 -0600

    Iconos en fondo oscuro

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
-import {Icono, Box, Btn, Saldo, Detalle } from "../UI/index.js";
+import {Icono, Iconotema, Box, Btn, Saldo, Detalle } from "../UI/index.js";
 import styled from "styled-components";
 
 const IconoMargin = styled(Icono)`
@@ -23,7 +23,7 @@ const Account = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
         <span>
-          <icono src={dinero} alt="Icono de saldo" />
+          <Iconotema src={dinero} alt="Icono de saldo" />
         </span>
         {toggleState ? (
           <Saldo>
--- src/imageFilter.js
+++ src/imageFilter.js
@@ -4,15 +4,15 @@ import salud from "./assets/images/salud.svg";
 import otros from "./assets/images/otros.svg";
 import transporte from "./assets/images/transporte.svg";
 import utilidades from "./assets/images/utilidades.svg";
-import {Icono} from "./Components/UI/index.js";
+import {Iconotema} from "./Components/UI/index.js";
 
 export default (type) => {
     const Images = {
-        Restaurante: <Icono src={alimentacion} alt="Restaurante"/>,
-        Salud: <Icono src={salud} alt="Salud"/>,
-        Transporte: <Icono src={transporte} alt="Transporte"/>,
-        Utilidades: <Icono src={utilidades} alt="Utilidades"/>,
-        default: <Icono src={otros} alt="Otros"/>
+        Restaurante: <Iconotema src={alimentacion} alt="Restaurante"/>,
+        Salud: <Iconotema src={salud} alt="Salud"/>,
+        Transporte: <Iconotema src={transporte} alt="Transporte"/>,
+        Utilidades: <Iconotema src={utilidades} alt="Utilidades"/>,
+        default: <Iconotema src={otros} alt="Otros"/>
 
     };
 
